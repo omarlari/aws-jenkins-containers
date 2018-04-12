ef15671edfd91afada328ef2307113630336e731
omarlari@gmail.com <omarlari@gmail.com>
2018-04-11 23:50:36 -0700

    moved to blue ocean jenkins container

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:8080 -u root -v /home/ec2-user/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker jenkins/jenkins
+docker run -d -p 80:8080 -u root -v /home/ec2-user/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker jenkinsci/blueocean
