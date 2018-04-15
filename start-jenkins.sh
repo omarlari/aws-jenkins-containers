6a208d5057c00454ba4c970acb5eb980f79bd6fe
omarlari@gmail.com <omarlari@gmail.com>
2018-04-14 18:39:00 -0700

    change image to blueocean

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:8080 -u root -v /home/ec2-user/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker jenkinsci/blueocean
+docker run -d -p 80:8080 -u root -v /home/ec2-user/jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker jenkinsci/blueocean:latest
